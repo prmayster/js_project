6f85e68cbdb7e987d14fcfc8c24fc17869d8794f
M.Shamrin <prmayster@gmail.com>
2022-03-12 22:25:19 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/* var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {
+
+};
+
+console.log(4/0);
+console.log('string'*9);
+
+let something;
+console.log(something);
+
+let person = {
+  name: "Jone",
+  age: 25,
+  isMarried: false
+};
+
+console.log(person.name);
+console.log(person['age']);
+
+let arr = ['plun.png','orange.jpeg','apple.jpeg'];
+console.log(arr[2]);
+
+// alert("Hello Word!")
+
+// let answer = confirm("Are you her?")
+// console.log(answer);
+
+let answer = prompt("Вам 18 лет", "Да")
+console.log(answer); */
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecket = false,
+    isClose = false;
+
+console.log(isChecket || !isClose);
